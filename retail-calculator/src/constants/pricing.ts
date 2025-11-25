b4329872d0e5133045aae45d9e53d92bd2ff8e01
amanvarmanz <31362555+amanvarmanz@users.noreply.github.com>
2025-11-25 21:48:54 +1300

    Create pricing.ts
    - Created Pricing matrix
    - Added with types

---
+++ retail-calculator/src/constants/pricing.ts
@@ -0,0 +1,17 @@
+export type RegionCode = "AUK" | "WLG" | "WAI" | "CHC" | "TAS"
+
+export const DISCOUNT : Record<number, number> = {    
+    1000: 0.03,
+    5000: 0.05,
+    7000: 0.07,
+    10000: 0.10,
+    50000: 0.15,
+}
+
+export const TAX : Record<RegionCode, number> = {
+    "AUK": 0.0685,
+    "WLG": 0.08,
+    "WAI": 0.0625,
+    "CHC": 0.04,
+    "TAS": 0.0825,
+}
