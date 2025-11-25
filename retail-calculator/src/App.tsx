246a4dac80e0f6dbc848bf6190c922d4e6228e2e
amanvarmanz <31362555+amanvarmanz@users.noreply.github.com>
2025-11-25 23:09:33 +1300

    Create Retail Calculator Form
    - Add Number of Items Input.
    - Add Price Per Item Input.
    - Add Region Input.
    - Create Region Select Pairings.

--- retail-calculator/src/App.tsx
+++ retail-calculator/src/App.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import '@mantine/core/styles.css';
-import { Button, MantineProvider, Text } from '@mantine/core';
+import { Button, MantineProvider, NumberInput, Select, Text } from '@mantine/core';
+import { SelectRegion } from './constants/pricing';
 
 function App() {
 
   return (
-    <>
       <MantineProvider>
-        <Text c={'black'} fz={100}> Hello World </Text>
-        <Button> Click Me </Button>
+        <div style={{ width:'25%', margin: 'auto',}}>
+          <Text c={'black'} fz={25}> Retail Calculator </Text>
+          <NumberInput  label="Number of Items" placeholder="Enter Number of Items" />
+          <NumberInput  label="Price Per Item" placeholder="Enter Price Per Item" />
+          <Select label="Select Region" placeholder={'Select Region'} data={SelectRegion} />
+        </div>
+
       </MantineProvider>
-    </>
   )
 }
 
